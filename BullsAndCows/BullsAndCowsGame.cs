20951f7f655d0db50b6a2cba53287c0de5e862fe
Jialong ZHANG <JZhang182@slb.com>
2020-11-19 16:18:18 +0800

    feat: implement 4A0B

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -16,11 +16,14 @@ namespace BullsAndCows
 
         public string Guess(string guess)
         {
-            return Compare(this.secret, guess);
+            var guessWithoutSpace = guess.Replace(" ", string.Empty);
+            return Compare(this.secret, guessWithoutSpace);
         }
 
         private string Compare(string secret, string guess)
         {
+            if (secret == guess)
+            { return "4A0B"; }
             return "0A0B";
         }
     }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -21,10 +21,34 @@ namespace BullsAndCowsTest
             var secretGenerator = new TestSecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
             //when
-            string answer = game.Guess("5678");
+            string answer = game.Guess("5 6 7 8");
             //then
             Assert.Equal("0A0B", answer);
         }
+
+        [Fact]
+        public void Should_return_4A0B_when_all_right()
+        {
+            //given
+            var secretGenerator = new TestSecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+            //when
+            string answer = game.Guess("1 2 3 4");
+            //then
+            Assert.Equal("4A0B", answer);
+        }
+
+        //[Fact]
+        //public void Should_return_0A4B_when_all_digits_are_right()
+        //{
+        //    //given
+        //    var secretGenerator = new TestSecretGenerator();
+        //    var game = new BullsAndCowsGame(secretGenerator);
+        //    //when
+        //    string answer = game.Guess("1 2 3 4");
+        //    //then
+        //    Assert.Equal("4A0B", answer);
+        //}
 
         public class TestSecretGenerator : SecretGenerator
         {
