09c13ba6ab0c9badb37ca243150377ecbeae72e3
Shishi Zhang <SZhang42@slb.com>
2020-11-19 18:22:37 +0800

    feat: implement test case: GenerateSecret should return a string has 4 numbers

---
+++ BullsAndCowsTest/SecretGeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using BullsAndCows;
+using Xunit;
+
+namespace BullsAndCowsTest
+{
+    public class SecretGeneratorTest
+    {
+        [Fact]
+        public void Should_Return_String_With_Length_Of_Four()
+        {
+            //given
+            var secretGenerator = new SecretGenerator();
+
+            //when
+            var secret = secretGenerator.GenerateSecret();
+
+            //then
+            Assert.Equal(4, secret.Length);
+        }
+    }
+}
