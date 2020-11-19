7e4ca5dac5fa12bf7dc09eb7f0fcca9adeaebeb2
Jialong ZHANG <JZhang182@slb.com>
2020-11-19 17:22:35 +0800

    test: add more test

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,5 +1,6 @@
 using BullsAndCows;
 using Xunit;
+using Moq;
 
 namespace BullsAndCowsTest
 {
@@ -26,39 +27,43 @@ namespace BullsAndCowsTest
             Assert.Equal("0A0B", answer);
         }
 
-        [Fact]
-        public void Should_return_4A0B_when_all_right()
+        [Theory]
+        [InlineData("4 3 2 1", "4321")]
+        [InlineData("4 3 1 2", "4312")]
+        public void Should_return_4A0B_when_all_right(string guess, string secret)
         {
             //given
-            var secretGenerator = new TestSecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
+            var mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(m => m.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
             //when
-            string answer = game.Guess("1 2 3 4");
+            string answer = game.Guess(guess);
             //then
             Assert.Equal("4A0B", answer);
         }
 
         [Theory]
-        [InlineData("4 3 2 1")]
-        [InlineData("4 3 1 2")]
-        [InlineData("2 3 4 1")]
-        public void Should_return_0A4B_when_all_digits_are_right(string guess)
+        [InlineData("4 3 2 1", "1234")]
+        [InlineData("4 3 1 2", "1234")]
+        [InlineData("2 3 4 1", "1234")]
+        public void Should_return_0A4B_when_all_digits_are_right(string guess, string secret)
         {
             //given
-            var secretGenerator = new TestSecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
+            var mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(m => m.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
             //when
             string answer = game.Guess(guess);
             //then
             Assert.Equal("0A4B", answer);
         }
 
-        public class TestSecretGenerator : SecretGenerator
-        {
-            public override string GenerateSecret()
-            {
-                return "1234";
-            }
-        }
+        //public class TestSecretGenerator : SecretGenerator
+        //{
+        //    public override string GenerateSecret()
+        //    {
+        //        return "1234";
+        //    }
+        //}
     }
 }
