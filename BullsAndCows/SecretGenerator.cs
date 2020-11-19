5524ce404cce612b9158725e955f10b014e69e07
Jialong ZHANG <JZhang182@slb.com>
2020-11-19 18:32:25 +0800

    feat: implement random generator

--- BullsAndCows/SecretGenerator.cs
+++ BullsAndCows/SecretGenerator.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BullsAndCows
 {
     public class SecretGenerator
     {
+        private static readonly List<int> StaticDigitList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
         public virtual string GenerateSecret()
         {
-            return string.Empty;
+            List<int> digitList = StaticDigitList;
+            var random = new Random();
+            string secret = string.Empty;
+            for (var i = 0; i < 4; i++)
+            {
+                int index = random.Next(0, digitList.Count + 1);
+                secret += digitList[index].ToString();
+                digitList.RemoveAt(index);
+            }
+
+            return secret;
         }
     }
 }
